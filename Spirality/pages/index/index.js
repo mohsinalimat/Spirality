d7e03ac5fa83420c9872ea9c212e15502c6fbd3a
Daniel <xcodeyang@gmail.com>
2018-05-25 17:31:16 +0800

    完成阵列绘图

--- Spirality/pages/index/index.js
+++ Spirality/pages/index/index.js
@@ -3,22 +3,25 @@
 Page({
   data: {
     windowWidth: 0, 
-    color: '#feeeed', // 画笔颜色
+    color: '#abc88b', // 画笔颜色
     count: 30,  // 阵列数量
     isPen: true,  // 是否使用钢笔
-    isAblePre: true,  // 可以使用上一步
-    isAbleNext: true, // 可以使用下一步
+    isAblePre: false,  // 可以使用上一步
+    isAbleNext: false, // 可以使用下一步
     isShowTool: true, // 左边工具栏展示
     isShowColorPicker: false, // 颜色选择器
     isShowCounter:false,
   },
-  // contextArray:[],
+  windowWidth:0,
+  windowHeight: 0,
   onLoad: function (options) {
     var res = wx.getSystemInfoSync()
-    this.setData({
-      windowWidth: res.windowWidth,
-    })
+    this.windowWidth = res.windowWidth;
+    this.windowHeight = res.windowHeight;
+    console.log(this);
   },
+
+
   touchstart(e) {
     if (e.touches.length > 1) { return; }
     
@@ -29,6 +32,9 @@ Page({
     this.context.setStrokeStyle(this.data.color)
     this.context.setLineWidth(1)
     this.context.setLineCap('round')
+
+    this.convertPointByCenterOfPoint(clientX, clientY, 1);
+    // console.log(newX, newY);
   },
   touchmove(e) {
     if (e.touches.length > 1) { 
@@ -42,9 +48,15 @@ Page({
     }
 
     let { clientX, clientY } = e.touches[0];
-    this.context.moveTo(this.prePointX, this.prePointY)
-    this.context.lineTo(clientX, clientY)
+
+    for(let i=0; i<this.data.count; i++) {
+      let [fromX, fromY] = this.convertPointByCenterOfPoint(this.prePointX, this.prePointY, i);
+      let [endX, endY] = this.convertPointByCenterOfPoint(clientX, clientY, i);
+      this.context.moveTo(fromX, fromY);
+      this.context.lineTo(endX, endY);
+    }
     this.context.stroke()
+
     this.prePointX = clientX;
     this.prePointY = clientY;
     
@@ -66,6 +78,8 @@ Page({
       isShowTool: true
     })
   },
+
+
   // 画笔类型
   penChange: function (e) {
     let isPen = !this.data.isPen;
@@ -116,26 +130,48 @@ Page({
   // 清楚画板
   clean(e){
     console.log('clean', e)
-
+    if (this.context) {
+      this.context.clearRect(10, 10, 1000, 275)
+    }
   },
   // 上一步
   rollBack(e) {
     console.log('rollBack', e)
-
   },
   // 下一步
   moveForward(e) {
     console.log('moveForward', e)
-
   },
   // 保存为图片
   save(e){
     console.log('save', e)
-
   },
   // 播放
   play(e){
     console.log('play', e)
+  },
 
+  // 用触摸点坐标和第index阵列 获取阵列的坐标
+  convertPointByCenterOfPoint(pointX, pointY, index) {
+    let centerX = this.windowWidth / 2.0;
+    let centerY = this.windowHeight / 2.0;
+    let increaseAngle = Math.PI * 2.0 * index / this.data.count;
+    console.log(centerX, centerY, pointX, pointY, index, increaseAngle);
+    let radius = Math.sqrt((centerX - pointX) * (centerX - pointX) + (centerY - pointY) * (centerY - pointY))
+    let angel = this.getAngleOnXAxisFromPoint(centerX, centerY, pointX, pointY) + increaseAngle;
+    let resultX = Math.cos(angel) * radius + centerX;
+    let resultY = Math.sin(angel) * radius + centerY;
+    return [resultX, resultY];
+  },
+
+  getAngleOnXAxisFromPoint(startX, startY, endX, endY) {
+    let xForwardStartX = startX + 10;
+    let a = endX - startX;
+    let b = endY - startY;
+    let c = xForwardStartX - startX;
+    let d = startY - startY;
+    let rads = Math.acos(((a * c) + (b * d)) / ((Math.sqrt(a * a + b * b)) * (Math.sqrt(c * c + d * d))));
+    return startY > endY ? (-rads) : rads;
   }
+
 })
